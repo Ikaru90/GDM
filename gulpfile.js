0575873895861a75eec7e465c83b7931e9785972
Ikaru90 <zabio90@gmail.com>
2019-05-04 12:57:15 +0300

    Added gulp tasks for server build.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const nodemon = require('gulp-nodemon');
+ 
+gulp.task('server:build', () =>
+    gulp.src('src/server/app.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('build'))
+);
+
+gulp.task('server:start', (done) => nodemon({
+    script: 'build/app.js',
+    watch: 'src/server',
+    tasks: ['server:build'],
+    done: done
+  })
+);
