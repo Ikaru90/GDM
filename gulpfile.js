44bd58ef20051e533a03daa5c2d8cf3d26bccc46
Ikaru90 <zabio90@gmail.com>
2019-05-05 22:57:32 +0300

    Added tasks for copy index.html & bundle.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const nodemon = require('gulp-nodemon');
- 
+
+gulp.task('copy:html', () => {
+  return gulp.src('src/client/index.html')
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('copy:js', () => {
+  return gulp.src('src/client/bundle.js')
+    .pipe(gulp.dest('build/public/'));
+});
+
 gulp.task('server:build', () =>
   gulp.src('src/server/app.js')
     .pipe(babel({
